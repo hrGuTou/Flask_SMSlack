6534f152143d4c23c42b8b6f6610b3bec1de4e6f
Haoran He <41132644+hrGuTou@users.noreply.github.com>
2018-12-03 23:28:34 -0500

    React Merge (#1)

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { HashRouter as Router, Route, NavLink } from 'react-router-dom';
+import SignUpForm from './pages/SignUpForm';
+import SignInForm from './pages/SignInForm';
+
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+
+        <Router basename="/login-ui/">
+        <div className="App">
+          <div className="App__Aside">
+            <div className="Product_Name">
+              <div>
+                <br/>
+                <h1>Welcome</h1>
+                <h1>to </h1>
+                <h1> SMSlack</h1>
+              </div>
+            </div>
+          </div>
+          <div className="App__Form">
+            <div className="PageSwitcher">
+                <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
+                <NavLink exact to="/sign-up" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+              </div>
+
+              <div className="FormTitle">
+                  <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or <NavLink exact to="/sign-up" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
+              </div>
+
+              <Route exact path="/sign-up" component={SignUpForm}>
+              </Route>
+              <Route path="/sign-in" component={SignInForm}>
+              </Route>
+          </div>
+        </div>
+      </Router>
+
+    );
+  }
+}
+
+export default App;
---
+++ src/pages/Mainpage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Mainpage extends Component{
+
+  render(){
+    return(
+      
+    );
+  }
+}
+export default Mainpage;
